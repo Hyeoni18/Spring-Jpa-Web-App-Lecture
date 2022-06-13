d0cbfa7f4e260147073e4a98631c7352412a8601
Hyeoni18 <gusgnl105@gmail.com>
2022-06-14 00:43:23 +0900

    회원가입 컨트롤러

---
+++ src/main/java/hello/springjpa/webapp/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package hello.springjpa.webapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity // 모든 설정을 직접 한다는 의미
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .mvcMatchers("/","/login","/sign-up","/check-email",
+                                "/check-email-token", "/email-login", "/check-email-login",
+                                "/login-link") // 권한 확인 없이 접근
+                        .permitAll()
+                        .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll() // GET만 허용
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
