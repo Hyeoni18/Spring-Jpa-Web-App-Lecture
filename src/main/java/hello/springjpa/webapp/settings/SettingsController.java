744d1e3b41be19f81e4423ba8422d408d9f6a1b0
Hyeoni18 <gusgnl105@gmail.com>
2022-07-09 00:23:11 +0900

    관심 주제 삭제

--- src/main/java/hello/springjpa/webapp/settings/SettingsController.java
+++ src/main/java/hello/springjpa/webapp/settings/SettingsController.java
@@ -143,18 +143,30 @@ public class SettingsController {
         return SETTINGS_TAGS_VIEW_NAME;
     }
 
-    @PostMapping("/settings/tags/add")
+    @PostMapping(SETTINGS_TAGS_URL+"/add")
     @ResponseBody
     public ResponseEntity addTag(@CurrentUser Account account, @RequestBody TagForm tagForm) {
         String title = tagForm.getTagTitle();
 //        Tag tag = tagRepository.findByTitle(title).orElseGet(() -> tagRepository.save(Tag.builder()
-//                        .title(tagForm.getTagTitle())
+//                        .title(title)
 //                .build())); // find 실패하면 저장해서 불러오기 (Optional 사용시)
         Tag tag = tagRepository.findByTitle(title);
         if(tag == null) {
-            tag = tagRepository.save(Tag.builder().title(tagForm.getTagTitle()).build());
+            tag = tagRepository.save(Tag.builder().title(title).build());
         }
         accountService.addTag(account, tag);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(SETTINGS_TAGS_URL+"/remove")
+    @ResponseBody
+    public ResponseEntity removeTag(@CurrentUser Account account, @RequestBody TagForm tagForm) {
+        String title = tagForm.getTagTitle();
+        Tag tag = tagRepository.findByTitle(title);
+        if(tag == null) {
+            return ResponseEntity.badRequest().build(); //없는 태그를 삭제할 경우
+        }
+        accountService.removeTag(account, tag);
         return ResponseEntity.ok().build();
     }
 }
