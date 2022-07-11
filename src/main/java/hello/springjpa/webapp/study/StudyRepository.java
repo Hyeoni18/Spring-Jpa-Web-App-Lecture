19627677af69e88a28b318bae0fddf2ef6a4f510
Hyeoni18 <gusgnl105@gmail.com>
2022-07-11 19:20:59 +0900

    스터디 개설

---
+++ src/main/java/hello/springjpa/webapp/study/StudyRepository.java
@@ -0,0 +1,10 @@
+package hello.springjpa.webapp.study;
+
+import hello.springjpa.webapp.domain.Study;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface StudyRepository extends JpaRepository<Study, Long> {
+    boolean existsByPath(String path);
+}
---
+++ src/main/java/hello/springjpa/webapp/study/StudyService.java
@@ -0,0 +1,21 @@
+package hello.springjpa.webapp.study;
+
+import hello.springjpa.webapp.domain.Account;
+import hello.springjpa.webapp.domain.Study;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class StudyService {
+
+    private final StudyRepository studyRepository;
+
+    public Study createNewStudy(Study study, Account account) {
+        Study newStudy = studyRepository.save(study);
+        newStudy.addManager(account);
+        return newStudy;
+    }
+}
