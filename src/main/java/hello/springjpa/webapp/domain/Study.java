ae3009e2f5fce0084916fcf12d0162f8b1ed007a
Hyeoni18 <gusgnl105@gmail.com>
2022-07-13 17:41:27 +0900

    스터디 조회

--- src/main/java/hello/springjpa/webapp/domain/Study.java
+++ src/main/java/hello/springjpa/webapp/domain/Study.java
@@ -1,5 +1,6 @@
 package hello.springjpa.webapp.domain;
 
+import hello.springjpa.webapp.account.UserAccount;
 import lombok.*;
 
 import javax.persistence.*;
@@ -7,6 +8,14 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+//view 변경할 때마다 발생하는 쿼리 살펴보기, 어차피 가져올 데이터라면 join해서 가져오기.
+// left outer join으로 연관 데이터 한 번에 조회 가능
+// entityManagerGraph 정의 -> repository에서 사용
+@NamedEntityGraph(name = "Study.withAll", attributeNodes = {
+        @NamedAttributeNode("tags"),
+        @NamedAttributeNode("zones"),
+        @NamedAttributeNode("managers"),
+        @NamedAttributeNode("members")})
 @Entity
 @Getter @Setter @EqualsAndHashCode(of = "id")
 @Builder @NoArgsConstructor @AllArgsConstructor
@@ -56,5 +65,20 @@ public class Study {
 
     public void addManager(Account account) {
         this.managers.add(account);
+    }
+
+    //가입 가능 여부
+    public boolean isJoinable(UserAccount userAccount) {
+        Account account = userAccount.getAccount();
+        return this.isPublished() && this.isRecruiting() //공개 가능, 모집 중, 멤버, 매니저가 아니면.
+                && !this.members.contains(account) && !this.managers.contains(account);
+    }
+
+    public boolean isMember(UserAccount userAccount) {
+        return this.members.contains(userAccount.getAccount());
+    }
+
+    public boolean isManager(UserAccount userAccount) {
+        return this.managers.contains(userAccount.getAccount());
     }
 }
--- src/main/java/hello/springjpa/webapp/study/StudyRepository.java
+++ src/main/java/hello/springjpa/webapp/study/StudyRepository.java
@@ -1,10 +1,14 @@
 package hello.springjpa.webapp.study;
 
 import hello.springjpa.webapp.domain.Study;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 public interface StudyRepository extends JpaRepository<Study, Long> {
     boolean existsByPath(String path);
+
+    @EntityGraph(value = "Study.withAll", type = EntityGraph.EntityGraphType.LOAD) //LOAD는 엔티티 매니저에 정의한 연관관계는 EAGER 모드로 가져오기. 나머지는 기본 fatch 타입에 따른다.
+    Study findByPath(String path);
 }
