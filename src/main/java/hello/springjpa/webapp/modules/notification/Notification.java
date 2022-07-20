4671288c75fa9002e64742119adb3a0bcd8f0ebb
Hyeoni18 <gusgnl105@gmail.com>
2022-07-21 00:02:50 +0900

    알림 도메인

---
+++ src/main/java/hello/springjpa/webapp/modules/notification/Notification.java
@@ -0,0 +1,33 @@
+package hello.springjpa.webapp.modules.notification;
+
+import hello.springjpa.webapp.modules.account.Account;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of="id")
+public class Notification {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private String message;
+
+    private boolean checked;
+
+    @ManyToOne
+    private Account account; //알림을 조회해야해. 해당 account 한테 알림메세지가 있는거니까. 조회해야해
+
+    private LocalDateTime createdLocalDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType notifivationType;
+}
---
+++ src/main/java/hello/springjpa/webapp/modules/notification/NotificationType.java
@@ -0,0 +1,6 @@
+package hello.springjpa.webapp.modules.notification;
+
+public enum NotificationType {
+
+    STUDY_CREATED, STUDY_UPDATED, EVENT_ENROLLMENT;
+}
